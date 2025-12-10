85a1fb82ffdf2889d672bbe74777db97f8308e84
Gerardo Alí Ferraro Schelijasch <76831691+stferraro@users.noreply.github.com>
2025-12-09 21:51:58 -0400

    Initial commit

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Odoo Module',
+    'version': '19.0.1.0.0',
+    'summary': 'A new odoo module',
+    'category': 'Other',
+    'author': 'Gerardo Alí Ferraro Schelijasch',
+    'website': 'https://soltecferr.com',
+    'license': 'OPL-1',
+    'depends': [
+        'base',
+    ],
+    "data": [],
+    'installable': True,
+    'application': True,
+}
